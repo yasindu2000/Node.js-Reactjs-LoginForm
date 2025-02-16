e971d3e3aceb2a94b68fb67799d9226285312981
yasindu2000 <yasindudahanayake@gmail.com>
2025-02-16 21:30:45 +0530

    create pages

---
+++ front-end/src/LoginValidation.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function LoginValidation() {
+  return (
+    <div>LoginValidation</div>
+  )
+}
+
+export default LoginValidation
---
+++ front-end/src/SignUpValidation.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SignUpValidation() {
+  return (
+    <div>SignUpValidation</div>
+  )
+}
+
+export default SignUpValidation
