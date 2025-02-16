3783b3ac066963e413da4c964b7921d82885ed65
yasindu2000 <yasindudahanayake@gmail.com>
2025-02-16 22:17:51 +0530

    signup validation form

--- front-end/src/Signup.jsx
+++ front-end/src/Signup.jsx
@@ -1,6 +1,6 @@
 import React,{useState} from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-
+import Validation from './SignUpValidation';
 import axios from "axios";
 
 function Signup() {
