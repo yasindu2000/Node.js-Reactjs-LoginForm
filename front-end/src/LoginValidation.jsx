c0cd2fd80b5648b65f4a594d50056bad1893344f
yasindu2000 <yasindudahanayake@gmail.com>
2025-02-17 00:35:13 +0530

    login validation

--- front-end/src/LoginValidation.jsx
+++ front-end/src/LoginValidation.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+function Validation(values) {
 
-function LoginValidation() {
-  return (
-    <div>LoginValidation</div>
-  )
+  let error = {}
+  const email_pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+  const password_pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,}$/
+
+  if(values.email === ""){
+
+      error.email = "Name should not be empty"
+  }
+  else if(!email_pattern.test(values.email)){
+      error.email = "email didn't match"
+  }else{
+
+    error.email = ""
+  }
+
+  if(values.password === ""){
+
+      error.password = "password should not be empty"
+  }else if(!password_pattern.test(values.password)){
+
+      error.password = "password didn't match"
+  }else{
+
+      error.password = ""
+  }
+
+  return error;
+
 }
 
-export default LoginValidation
+
+export default Validation;
